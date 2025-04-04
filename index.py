870288ac2e796c3b24158a609c8cf8ff6e46fd16
PrabhuAbhishek24 <168215792+PrabhuAbhishek24@users.noreply.github.com>
2025-04-04 10:34:08 +0530

    Update index.py

--- index.py
+++ index.py
@@ -642,8 +642,8 @@ elif selected_section == "CSV Content Generation":
             if df is not None:
                 st.subheader("CSV Data Preview")
                 st.dataframe(df)  # Display CSV as table
-            # Horizontal line before download options
-            st.markdown("---")
+       # Horizontal line before download options
+       st.markdown("---")
 
                 # Provide a button to download the CSV file
                 csv_buffer = io.StringIO()
